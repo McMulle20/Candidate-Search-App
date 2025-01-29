31aa8b5b2f4ce42b57798f925f47b053ec3e30f6
mcmul <mcmulle20.laxwi@gmail.com>
2025-01-28 20:02:10 -0600

    render

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './env',
+  envDir: './env', // Specify the environment variable directory
   plugins: [react()],
+  server: {
+    port: parseInt(process.env.PORT || '3001', 10), // Default to 3001 if process.env.PORT is undefined
+    host: true, // Ensures it binds to all network interfaces (important for Render)
+  },
 });
